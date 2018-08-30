efc663407c7c3e39593866d9e356023f233d234c
Peter Vigh <vpeti88@gmail.com>
2018-08-30 23:53:09 +0200

    Improve code to improve stability

--- colorClock.js
+++ colorClock.js
@@ -3,11 +3,11 @@ function colorClock() {
   var hours = date.getHours();
   var mins = date.getMinutes();
   var secs = date.getSeconds();
-  var mHours = 256-Math.round(256/24)*hours;
-  var mMins = 256-Math.round(256/60)*mins;
-  var mSecs = Math.round(256/60)*secs;
+  var mHours = 255 - Math.round(256 / 24) * hours;
+  var mMins = 255 - Math.round(256 / 60) * mins;
+  var mSecs = Math.round(256 / 60) * secs;
   
-  var hexColor = '#' + mHours.toString(16) + mMins.toString(16) + mSecs.toString(16);
+  var rgbColor = 'rgb(' + mHours + ', ' + mMins + ', ' + mSecs + ')';
   
   if (secs < 10) {
     secs = '0' + secs;
@@ -18,11 +18,11 @@ function colorClock() {
   if (hours < 10) {
     hours = '0' + hours;
   }
-  var clockFace = hours+':'+mins+':'+secs;
-  
-  setTimeout(function(){
-    colorClock();
-  },1000);
-  document.body.style.background = hexColor;
+  var clockFace = hours + ':' + mins + ':' + secs;
+
+  setTimeout(function() {
+    colorClock ();
+  }, 1000);
+  document.body.style.background = rgbColor;
   document.getElementById('clock').innerHTML = clockFace;
 }
